f1f93767df6b4fc12359883354881ca9da6306ff
Manuela Ienuso <lunaienuso@libero.it>
2022-05-02 13:36:20 +0200

    Added main index, style, script and finish base app

---
+++ script.js
@@ -0,0 +1,13 @@
+/*Vue Hello
+Stampare a schermo un messaggio all'interno di un h1 utilizzando i data
+
+Bonus:
+Aggiungere alla pagina un'immagine presa anch'essa da un data*/
+
+const app = new Vue ({
+  el: "#app",
+
+  data: {
+    messaggio: "Ciao!",
+  }
+})
